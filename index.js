bb32cc82d513f569ef06801af0a10e59d26d2027
Ian McCausland <ian.mccausland@tophat.com>
2020-02-05 16:09:58 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,72 @@
+const alfy = require("alfy");
+
+function formatList(data, joinWith = ",", wrapper = "", prefix = "") {
+    return data
+        .split("\n")
+        .filter(item => item.trim() !== "")
+        .map(item => `${wrapper}${prefix}${item.trim()}${wrapper}`)
+        .join(joinWith);
+}
+
+function formatGql(server, data) {
+    const prefix = `production:${server.toLowerCase()}-`;
+
+    return formatList(data, ",", '\\"', prefix);
+}
+
+const SERVER_CODES = ["us", "ca"];
+const cleanedInput = alfy.input
+    .replace(/production:us-/g, "")
+    .replace(/production:ca-/g, "");
+
+const commaSeparatedOutput = formatList(cleanedInput);
+const commaSeparatedInSingleQuotes = formatList(cleanedInput, ",", "'");
+const filterForRemoval = formatList(cleanedInput, "\n");
+const [usServerGql, caServerGql] = SERVER_CODES.map(server =>
+    formatGql(server, cleanedInput)
+);
+
+const list = [
+    {
+        title: "Inventory IDs Formatted for Removal",
+        subtitle: filterForRemoval,
+        text: {
+            copy: filterForRemoval,
+            largetype: filterForRemoval
+        }
+    },
+    {
+        title: "Unmodified Comma-Separated Output",
+        subtitle: commaSeparatedOutput,
+        text: {
+            copy: commaSeparatedOutput,
+            largetype: commaSeparatedOutput
+        }
+    },
+    {
+        title: "Comma-Separated Output in Single Quotes",
+        subtitle: commaSeparatedInSingleQuotes,
+        text: {
+            copy: commaSeparatedInSingleQuotes,
+            largetype: commaSeparatedInSingleQuotes
+        }
+    },
+    {
+        title: "Graph QL US Server Output (Comma-Separated)",
+        subtitle: usServerGql,
+        text: {
+            copy: usServerGql,
+            largetype: usServerGql
+        }
+    },
+    {
+        title: "Graph QL CA Server Output (Comma-Separated)",
+        subtitle: caServerGql,
+        text: {
+            copy: caServerGql,
+            largetype: caServerGql
+        }
+    }
+];
+
+alfy.output(list);
