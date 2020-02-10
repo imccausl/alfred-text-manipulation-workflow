7b484da0f28e4c31c46a387f61e761a4aeeffe47
Ian McCausland <ian.mccausland@tophat.com>
2020-02-10 11:35:21 -0500

    Parse tab delimited table in clipboard into new-line separated lists of each column.

--- csv.js
+++ csv.js
@@ -1,1 +1,43 @@
 const alfy = require("alfy");
+const parse = require("csv-parse/lib/sync");
+
+function convertToAlfredOutput(obj) {
+  const items = [];
+
+  Object.keys(obj).forEach(key => {
+    const column = obj[key].join("\n");
+
+    items.push({
+      title: `List of items in column '${key}'`,
+      subtitle: `${obj[key].length} items in list: ${column}`,
+      arg: column,
+      text: {
+        copy: column,
+        largetype: column
+      }
+    });
+  });
+
+  return items;
+}
+
+const csv_config = {
+  delimiter: "\t",
+  trim: true,
+  columns: true
+};
+
+const records = parse(alfy.input, csv_config);
+const columnLists = records.reduce((obj, record) => {
+  Object.keys(record).forEach(key => {
+    if (!obj[key]) {
+      obj[key] = [record[key]];
+    } else {
+      obj[key].push(record[key]);
+    }
+  });
+
+  return obj;
+}, {});
+
+alfy.output(convertToAlfredOutput(columnLists));
